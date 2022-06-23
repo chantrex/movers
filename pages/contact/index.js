e76bfc3cbae24486282a00eb432ae0b4a930dbb5
ChristopherChantres <chris.chantres@gmail.com>
2022-06-22 21:26:08 -0400

    Redirections between home and contact pages are working ok

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,3 +1,14 @@
+import Link from "next/link"
+
 export default function ContactUs() {
-  return <h1>Contact us!</h1>
+  return (
+    <>
+      <h1>Get in touch!</h1>
+      <h2>
+        <Link href="/">
+          Go back to Home!
+        </Link>
+      </h2>
+    </>
+  );
 }
