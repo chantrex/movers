7ede6cddaaf67ce6ac644ef7abb02f4f9cb2aeb6
ChristopherChantres <chris.chantres@gmail.com>
2022-06-22 22:06:33 -0400

    Components folder is created to work with css

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,14 +1,30 @@
-import Link from "next/link"
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function ContactUs() {
   return (
     <>
-      <h1>Get in touch!</h1>
-      <h2>
-        <Link href="/">
-          Go back to Home!
-        </Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>Movers | Contact</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+        <h1>Get in touch!</h1>
+        <h2>
+          <Link href="/">
+            Go back to Home!
+          </Link>
+        </h2>
+      </Layout>
+      
     </>
   );
 }
