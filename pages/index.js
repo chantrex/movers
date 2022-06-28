757c2c3305d427f55318544e229454dada7005da
ChristopherChantres <chris.chantres@gmail.com>
2022-06-28 14:59:24 -0400

    Gif is working as a background in the home page

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,11 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <Navbar />
-      <section className={utilStyles.headingMd}>
-        <p>Pulga Movers. Because we move!</p>
+      <section className={utilStyles.container1}>
+        <h1>Pulga Movers. Because changes are good!</h1>
+        <div className={utilStyles.videoContainer}>
+          <img src="https://media2.giphy.com/media/UTj6uNKzYFmJGbKdgH/giphy.gif?cid=ecf05e4796z486iumakdlkts123fzlpxw8ibh6odsb3wj9sv&rid=giphy.gif&ct=g" alt="" />
+        </div>
       </section>
     </Layout>
   );
