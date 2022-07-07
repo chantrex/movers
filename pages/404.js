c86d6db95fb9c3cd8145027e40c1df96c743c5ee
ChristopherChantres <chris.chantres@gmail.com>
2022-07-07 12:57:33 -0400

    404 page has been updated

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,18 @@
+import styles from '../styles/error404.module.css';
+import Link from 'next/link';
+
 export default function Page404() {
   return (
-    <h1>404</h1>
+    <>
+      <section className={styles.sectionContainer}>
+        <main className={styles.sectionMain}>
+          <h1>404</h1>
+          <p>- Page Not Found -</p>
+          <Link href="/">
+            <a className={styles.goBack}>Go back home</a>
+          </Link>
+        </main>
+      </section>
+    </>
   );
 }
